3b389e678008507e642845b9ca5fc68a148c21c5
Randy Panopio <redaugust7@hotmail.com>
2019-10-03 11:32:02 -0700

    Initial commit
    setup project

---
+++ Assignment1/directives/nav_bar.php
@@ -0,0 +1,23 @@
+<!-- Top navigation bar -->
+<div id="main-menu-bar-container">
+    <div id="main-menu-bar">
+
+        <!-- Logo -->
+        <a href="index.php" id="main-menu-bar-left">
+            <img id="main-menu-bar-logo" src="img/coopconnect.png" alt="Logo">
+        </a>
+
+        <!-- right side of menu bar -->
+        <div id="main-menu-bar-right">
+
+            <!-- sign up prompt -->
+            <a  class="button" id="sign-up-button" href="sign_up.php">
+                Sign up
+            </a>
+            <!-- log in / sign out button -->
+            <a  class="button" id="log-in-button" href="log_in.php">
+                Log in
+            </a>
+        </div>
+    </div>
+</div>
---
+++ Assignment1/index.php
@@ -0,0 +1,21 @@
+<!-- IAT 352 Randy Panopio -->
+
+<!-- add header - start -->
+<?php require("directives/header.php"); ?>
+<!-- add menu bar -->
+<?php require("directives/nav_bar.php"); ?>
+
+
+<div id="content-container-welcome">
+
+<h1>Welcome</h1>
+
+  <div class="cards">
+          <a  class="" id="" href="">
+              See modules
+          </a>
+  </div>
+</div>
+
+<!-- add footer - close  -->
+<?php require("directives/footer.php"); ?>
